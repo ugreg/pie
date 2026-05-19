a4103dc113955992f5c8dccbe7ee6bebb0919b4c
ugreg <8151045+ugreg@users.noreply.github.com>
2026-05-19 15:40:29 -0400

    actualizar error logs

--- permissions/index.ts
+++ permissions/index.ts
@@ -11,7 +11,7 @@ export default function (pi: ExtensionAPI) {
   const config = new Config();
   pi.on("session_start", async (_event, ctx) => {
     if (!existsSync(Config.FILE_PATH)) {
-      ctx.ui.notify(`\nPi Permissions lite - required file ${Config.FILE_PATH} not found. Please create it.`, "error");
+      ctx.ui.notify(`\nPi Permissions lite ERROR: required file ${Config.FILE_PATH} not found. Please create it.`, "error");
     } else {
       ctx.ui.notify("Pi Permissions lite extension loaded!\n", "info");
     }
